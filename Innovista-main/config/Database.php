01ac3653123f54664c4e28d411c9478f250cec7e
jeyapragash <kishojeyapragash15@gmail.com>
2025-09-10 20:24:48 +0530

    denu

--- Innovista-main/config/Database.php
+++ Innovista-main/config/Database.php
@@ -1,7 +1,7 @@
 <?php
 class Database {
-    private $host = '127.0.0.1';       // better than 'localhost' in XAMPP
-    private $port = '3307';            // change if your MySQL uses 3307
+    private $host = '127.0.0.1';  
+    private $port = '3307';          
     private $db_name = 'innovista';
     private $username = 'root';
     private $password = '';
